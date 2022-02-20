41408d21b72aecd5031d62d8252fefa2c6e7239c
zakhep66 <sur.mikhael@gmail.com>
2022-02-21 02:22:25 +0300

    add Cards model

--- table_game/models.py
+++ table_game/models.py
@@ -27,14 +27,36 @@ CARD_TYPE = (
 )
 
 
-class Card(models.Model):
+class CardSpell(models.Model):
 	card_spell = models.CharField(verbose_name="заклинание карты", max_length=100, unique=True)
 	card_text = models.TextField(verbose_name="суть заклинания")
 	card_stage = models.CharField(verbose_name="этап заклинания", choices=CARD_STAGE, max_length=20)
 	card_type = models.CharField(verbose_name="тип карты", choices=CARD_TYPE, max_length=20)
 
 	class Meta:
-		verbose_name = "Card"
+		verbose_name = "Card-spell"
 
 	def __str__(self):
 		return self.card_spell
+
+
+class CardTreasure(models.Model):
+	card_treasure = models.CharField(verbose_name="название карты", max_length=30)
+	card_text = models.CharField(verbose_name="тест карты", max_length=100)
+
+	class Meta:
+		verbose_name = "Card-treasure"
+
+	def __str__(self):
+		return self.card_treasure
+
+
+class CardDeath(models.Model):
+	card_death = models.CharField(verbose_name="название карты", max_length=30)
+	card_text = models.CharField(verbose_name="текст карты", max_length=100)
+
+	class Meta:
+		verbose_name = "Card-death"
+
+	def __str__(self):
+		return self.card_death
